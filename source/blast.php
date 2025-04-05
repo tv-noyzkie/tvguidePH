10217da62f0a6b8fa7232a6bd5a16ad3641723df
tv-noyzkie <tv.noyzkie@gmail.com>
2025-04-05 18:11:42 +0800

    Update blast.php

--- source/blast.php
+++ source/blast.php
@@ -1,13 +1,5 @@
 <?php
-
-function get_content($url) {
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    $output = curl_exec($ch);
-    curl_close($ch);
-    return $output;
-}
+require_once 'utils.php';
 
 define('BASE_URL', 'https://epg.tapdmv.com/calendar');
 
