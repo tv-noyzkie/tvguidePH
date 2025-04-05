e15127fb9b6be96e458bf8a78e418b9ba95e2f0e
tv-noyzkie <tv.noyzkie@gmail.com>
2025-04-05 18:10:43 +0800

    Create utils.php

---
+++ source/utils.php
@@ -0,0 +1,12 @@
+<?php
+
+function get_content($url) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $output = curl_exec($ch);
+    curl_close($ch);
+    return $output;
+}
+
+?>
