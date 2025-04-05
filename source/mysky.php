9bf6b364e99b6f21cfda7bb4d2683b76a7306493
tv-noyzkie <tv.noyzkie@gmail.com>
2025-04-05 18:13:49 +0800

    Update mysky.php

--- source/mysky.php
+++ source/mysky.php
@@ -70,7 +70,7 @@ function fetch_mysky_schedule($channel_site_id, $date) {
 }
 
 function generate_mysky_epg() {
-    echo "Starting to generate MySky EPG...\n";
+    echo "Starting to generate MySky EPG for the next 2 hours...\n";
     $channels = fetch_mysky_channels();
     if (empty($channels)) {
         echo "No MySky channels found. Exiting.\n";
@@ -88,22 +88,26 @@ function generate_mysky_epg() {
         $xml .= "</channel>\n";
     }
 
-    $today = new DateTime('now', new DateTimeZone('Asia/Manila'));
-    $tomorrow = (new DateTime('now', new DateTimeZone('Asia/Manila')))->modify('+1 day');
-    $dates = [$today, $tomorrow];
+    $now = new DateTime('now', new DateTimeZone('Asia/Manila'));
+    // We will only fetch for the current hour and the next
+    $dates = [$now];
 
     foreach ($channels as $channel) {
         echo "Fetching schedule for MySky channel: " . htmlspecialchars($channel['name']) . " (ID: " . htmlspecialchars($channel['site_id']) . ")...\n";
         foreach ($dates as $date) {
             $schedule = fetch_mysky_schedule($channel['site_id'], $date);
             if (!empty($schedule)) {
+                $two_hours_later = (clone $now)->modify('+2 hours');
                 foreach ($schedule as $program) {
-                    $xml .= "<programme start=\"" . htmlspecialchars($program['start']) . "\" stop=\"" . htmlspecialchars($program['stop']) . "\" channel=\"" . htmlspecialchars($channel['site_id']) . "\">\n";
-                    $xml .= "<title lang=\"" . htmlspecialchars($channel['lang']) . "\">" . htmlspecialchars($program['title']) . "</title>\n";
-                    if (!empty($program['description'])) {
-                        $xml .= "<desc lang=\"" . htmlspecialchars($channel['lang']) . "\">" . htmlspecialchars($program['description']) . "</desc>\n";
+                    $program_start = DateTime::createFromFormat('YmdHis O', $program['start']);
+                    if ($program_start >= $now && $program_start < $two_hours_later) {
+                        $xml .= "<programme start=\"" . htmlspecialchars($program['start']) . "\" stop=\"" . htmlspecialchars($program['stop']) . "\" channel=\"" . htmlspecialchars($channel['site_id']) . "\">\n";
+                        $xml .= "<title lang=\"" . htmlspecialchars($channel['lang']) . "\">" . htmlspecialchars($program['title']) . "</title>\n";
+                        if (!empty($program['description'])) {
+                            $xml .= "<desc lang=\"" . htmlspecialchars($channel['lang']) . "\">" . htmlspecialchars($program['description']) . "</desc>\n";
+                        }
+                        $xml .= "</programme>\n";
                     }
-                    $xml .= "</programme>\n";
                 }
             } else {
                 echo "No schedule found for " . htmlspecialchars($channel['name']) . " on " . $date->format('Y-m-d') . ".\n";
@@ -113,7 +117,7 @@ function generate_mysky_epg() {
 
     $xml .= "</tv>\n";
 
-    $epg_path = 'output/individual/mysky_' . date('Ymd') . '.xml';
+    $epg_path = 'output/individual/mysky_' . date('YmdHis') . '_2hr.xml'; // Include timestamp for more frequent runs
     echo "Writing MySky EPG data to " . htmlspecialchars($epg_path) . "...\n";
 
     $dom = new DOMDocument();
